8d6a5e57a11744a13942902ec131f2d036c8be56
daiyan52 <rehanalamkkr@gmail.com>
2024-02-28 00:02:26 +0530

    done

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from testapp.models import Contact
-# Register your models here.
-
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email','message']
-
-
-admin.site.register(Contact, ContactAdmin)
--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .forms import ContactForm
 
 class  homeView(TemplateView):
     template_name = "pages/home.html"
